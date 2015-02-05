f27edd9460d9d8a49d0c9beeb850113f604e048e
lutaoact <lutaoact@hotmail.com>
2015-02-05 15:44:36 +0800

    convert_srt_to_json.pl: $src_path = "local_data/L01-54"

--- scripts/mongo_migration.js
+++ scripts/mongo_migration.js
@@ -1,1 +1,3 @@
 db.sentences.remove({lessonNo: {$in: [28, 29, 32, 33]}});//2015-02-03 4个lesson的内容有误
+
+db.sentences.update({lessonNo: 12, sentenceNo: 13}, {$set: {english: "He doesn't want cream."}});//2015-02-05 一个字段有误
