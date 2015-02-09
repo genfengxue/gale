94c2632ed76c0a706d212526d30a87dee1ea1914
lutaoact <lutaoact@hotmail.com>
2015-02-09 19:53:32 +0800

    convert_key_point_txt_to_json.pl: completed

--- scripts/convert_key_point_txt_to_json.pl
+++ scripts/convert_key_point_txt_to_json.pl
@@ -2,6 +2,7 @@ use warnings;
 use strict;
 
 use lib 'scripts';
+use Data::Dumper;
 use Gale qw/read_file write_file json_encode/;
 
 my $TXT_DIR = 'local_data/key_point_txt';
@@ -13,15 +14,20 @@ my @files = glob "*";
 
 for my $file (@files) {
   my $lessonNo = Gale::getLessonNo($file);
-#  print $lessonNo;
+  my $result = {};
   my $file_content = read_file($file);
   while ($file_content =~ m#(\d+)\n.*?`(.+?)`.*?<k>(.*?)</k>#sg) {
     next unless $3;
-    my ($sentenceNo, $key, $key_point) = ($1, $2, $3);
-    print "*" x 20, "\n";
-    print "$1#####$2#####$3\n";
-    print "#" x 20, "\n";
+    my ($sentenceNo, $key, $text) = (int($1), $2, $3);
+    push @{ $result->{$lessonNo}->{$sentenceNo}->{$key} }, $text;
   }
-#  print $file_content;
+  write_file("../../$KP_DIR/$file.json", json_encode($result));
+#  print Dumper $result;
 }
 
+__END__
+#  print $lessonNo;
+
+#    print "*" x 20, "\n";
+#    print "$1#####$2#####$3\n";
+#    print "#" x 20, "\n";
