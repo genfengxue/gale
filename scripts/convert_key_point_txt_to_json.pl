4b67d4338e6024f480180c0d872736da68d840a2
lutaoact <lutaoact@hotmail.com>
2015-02-11 16:51:26 +0800

    scripts/convert_key_point_txt_to_json.pl: build result for one file

--- scripts/convert_key_point_txt_to_json.pl
+++ scripts/convert_key_point_txt_to_json.pl
@@ -16,12 +16,8 @@ for my $file (@files) {
   my $lessonNo = Gale::getLessonNo($file);
   my $result = {};
   my $fileContent = read_file($file);
-#  while ($fileContent =~ m#(\d+)\n.*?`(.+?)`.*?<k>(.*?)</k>#sg) {
   while ($fileContent =~ m#(\d+)\n(.*?)\n<k>(.*?)</k>#sg) {
     next unless $3;
-#    print "*" x 20, "\n";
-#    print "$1#####$2#####$3\n";
-#    print "#" x 20, "\n";
     my ($sentenceNo, $sentence, $text) = (int($1), $2, $3);
     my @snippets = split /(`.*?`)/, $sentence;
 #    print scalar @snippets;
@@ -33,8 +29,6 @@ for my $file (@files) {
         $keyStr =~ s/[^a-zA-Z0-9_-]+/ /g; #将非法单词字符都转为空格
         $keyStr =~ s/^\s+|\s+$//g; #去掉首尾空白
         my @keys = split /\s+/, $keyStr; #根据空白切割成单词
-#        my $numKeys = scalar @keys;
-#        print scalar @keys, "\n";
         push @keyIndexes, ++$from for @keys;
 #        for (@keys) {
 #          print "$_\n";
@@ -47,14 +41,12 @@ for my $file (@files) {
       }
     }
     print "@keyIndexes\n";
-#    print join "\n", @snippets;
-#    while ($sentence =~ /`(.*?)`/g) {
-#      print "*********$1*********\n"
-#    }
+    my $key = join ",", @keyIndexes;
+    push @{ $result->{$lessonNo}->{$sentenceNo}->{$key} }, "1";
 #    push @{ $result->{$lessonNo}->{$sentenceNo}->{$key} }, $text;
   }
 #  write_file("../../$KP_DIR/$file.json", json_encode($result));
-#  print Dumper $result;
+  print Dumper $result;
 }
 
 __END__
@@ -64,3 +56,4 @@ __END__
 #    print "$1#####$2#####$3\n";
 #    print "#" x 20, "\n";
 # Pam,give me a `.garden .sal-ad.` and `a gril-led fish.` appetizer.
+#    print join "\n", @snippets;
