35ffe3ff00ecf0efaf8bd35f2156efe438669c13
lutaoact <lutaoact@hotmail.com>
2015-03-27 21:11:07 +0800

    copy_lesson12_to_lesson999: add comment

--- scripts/copy_lesson12_to_lesson999.js
+++ scripts/copy_lesson12_to_lesson999.js
@@ -1,3 +1,6 @@
+//将lesson12的数据复制一份给lesson999
+//删除_id，在存储的时候，会自动新建记录
+
 db.sentences.find({lessonNo: 12}).sort({sentenceNo: 1}).forEach(function(sentence) {
   print(sentence._id + " " + sentence.sentenceNo + " " + sentence.lessonNo);
   delete sentence._id;
