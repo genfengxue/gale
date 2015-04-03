0e6a87d8ea1de78c682085519e5590b2a9297f1d
lutaoact <lutaoact@hotmail.com>
2015-04-04 00:03:11 +0800

    convert GBK and UTF-16 to UTF-8

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -8,3 +8,8 @@ for i in $(file lesson??.txt | grep 'UTF-16' | awk 'BEGIN{FS=":"} {print $1}'); 
 gsed '1~2!G' -i *.txt
 #转为windows风格的断行，这里应该调查一下，可否通过sed直接完成
 unix2dos *.txt
+
+file $(git diff --name-only 149c917043eb0778d3b1205d6e23dc80e2356f1f..HEAD)
+for i in $(file $(git diff --name-only 149c917043eb0778d3b1205d6e23dc80e2356f1f..HEAD) | grep 'ISO-8859' | awk 'BEGIN{FS=":"} {print $1}'); do iconv -f GBK -t UTF-8 $i > ${i/.srt/.new.srt}; done
+
+for i in $(file $(git diff --name-only 149c917043eb0778d3b1205d6e23dc80e2356f1f..HEAD) | grep 'UTF-16' | awk 'BEGIN{FS=":"} {print $1}'); do iconv -f UTF-16 -t UTF-8 $i > ${i/.srt/.new.srt}; done
