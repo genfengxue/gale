572c57ce4115a3f52a5534a5b9b8acd5a6b8caae
lutaoact <lutaoact@hotmail.com>
2015-04-04 06:26:20 +0800

    add_space_suffix.pl reset $i = 0

--- scripts/add_space_suffix.pl
+++ scripts/add_space_suffix.pl
@@ -3,8 +3,14 @@ use strict;
 
 $^I = '';
 
+my $old_name = '';
 my $i = 0;
 while (<>) {
+  if ($old_name ne $ARGV) {
+    $old_name = $ARGV;
+    $i = 0;
+  }
+
   if ($i % 5 == 2) {
     s/(".+?")(\w)/$1 $2/g;
     s/([.,?!;\)\]\}])(\w)/$1 $2/g;
