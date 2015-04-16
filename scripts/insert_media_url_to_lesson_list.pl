ed6dc9ca95ee12db6e9835a219fd5df225fce35f
lutaoact <lutaoact@hotmail.com>
2015-04-16 11:51:26 +0800

    insert_media_url_to_lesson_list.pl

--- scripts/insert_media_url_to_lesson_list.pl
+++ scripts/insert_media_url_to_lesson_list.pl
@@ -1,14 +1,31 @@
 use warnings;
 use strict;
 
-# perl scripts/insert_media_url_to_lesson_list.pl local_data/others/2_original_lesson_list.txt > local_data/lesson_list/2_lesson_list.txt
+use lib 'scripts';
+use Gale qw/getCourseNoFromName/;
+
+# perl scripts/insert_media_url_to_lesson_list.pl de
+# perl scripts/insert_media_url_to_lesson_list.pl nceone
+my $courseName = $ARGV[0];
+die "you must give courseName: [de/nceone]" unless $courseName;
+
+my $courseNo = getCourseNoFromName($courseName);
+die "courseName is illegal" unless $courseNo;
+
+my $originalFile = sprintf 'local_data/others/%s_original_lesson_list.txt', $courseNo;
+my $outputFile   = sprintf 'local_data/lesson_list/%s_lesson_list.txt',     $courseNo;
+
+open my $inFh, '<', $originalFile or die $!;
+open my $outFh, '>', $outputFile or die $!;
 
 my $count = 0;
-while (<>) {
+while (my $line = <$inFh>) {
   $count++;
-  unless ($count % 4) {
-    printf "http://7u2qm8.com1.z0.glb.clouddn.com/de%03s.jpg\n", int($count / 4);
-    printf "http://7u2qm8.com1.z0.glb.clouddn.com/de%03s.mp4\n", int($count / 4);
+  print $outFh $line;
+  if ($count % 4 == 3) {
+    printf $outFh "http://7u2qm8.com1.z0.glb.clouddn.com/%s%03s.jpg\n", $courseName, int($count / 4) + 1;
+    printf $outFh "http://7u2qm8.com1.z0.glb.clouddn.com/%s%03s.mp4\n", $courseName, int($count / 4) + 1;
   }
-  print;
 }
+close $inFh or die $!;
+close $outFh or die $!;
