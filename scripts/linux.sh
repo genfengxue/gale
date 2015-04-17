2073a755e73158f8ab3aaee9ed31ea095164d240
lutaoact <lutaoact@hotmail.com>
2015-04-17 11:24:09 +0800

    srt2utf8

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -17,11 +17,20 @@ for i in $(file $(git diff --name-only 149c917043eb0778d3b1205d6e23dc80e2356f1f.
 #批量重命名
 for i in $(ls nce1lesson???.txt); do git mv $i "nceone${i:10:3}.txt"; done
 
-#字幕处理
+#字幕处理GBK
 for i in $(file * | grep 'ISO-8859' | awk 'BEGIN{FS=":"} {print $1}')
 do
   j="${i}_tmp"
   iconv -f GBK -t UTF-8 $i > $j
+  mv $j $i
+  dos2unix $i
+done
+
+#字幕处理UTF-16
+for i in $(file * | grep 'UTF-16' | awk 'BEGIN{FS=":"} {print $1}')
+do
+  j="${i}_tmp"
+  iconv -f UTF-16 -t UTF-8 $i > $j
   mv $j $i
   dos2unix $i
 done
---
+++ scripts/srt2utf8.sh
@@ -0,0 +1,17 @@
+#字幕处理GBK
+for i in $(file * | grep 'ISO-8859' | awk 'BEGIN{FS=":"} {print $1}')
+do
+  j="${i}_tmp"
+  iconv -f GBK -t UTF-8 $i > $j
+  mv $j $i
+  dos2unix $i
+done
+
+#字幕处理UTF-16
+for i in $(file * | grep 'UTF-16' | awk 'BEGIN{FS=":"} {print $1}')
+do
+  j="${i}_tmp"
+  iconv -f UTF-16 -t UTF-8 $i > $j
+  mv $j $i
+  dos2unix $i
+done
