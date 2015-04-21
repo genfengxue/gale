124227c336ef19e41615e4a8bd9ea7b9090676e4
lutaoact <lutaoact@hotmail.com>
2015-04-21 11:27:26 +0800

    user_info_readable.txt

--- scripts/create_user_info_json.pl
+++ scripts/create_user_info_json.pl
@@ -12,17 +12,24 @@ my $map = {
 };
 
 my $results;
+open my $out_fh, '>', 'local_data/others/user_info_readable.txt' or die $!;
+
 while (my $line = <>) {
   chomp $line;
   my ($userNo, $nickname, $roleName) = split /\s+/, $line;
   my $role = $map->{$roleName};
+  my $password = substr sha1_hex($userNo), 0, 6;
+
   push @{$results}, +{
     userNo => $userNo,
     nickname => $nickname,
     role => $role,
-    password => substr sha1_hex($userNo), 0, 6
-  }
+    password => $password,
+  };
+  print $out_fh (join "  ", $userNo, $password, $nickname, $roleName), "\n";
 }
+
+close $out_fh or die $!;
 print Dumper $results;
-print scalar @{$results};
-write_file('local_data/others/user_info.json', json_encode($results));
+#print scalar @{$results};
+#write_file('local_data/others/user_info.json', json_encode($results));
