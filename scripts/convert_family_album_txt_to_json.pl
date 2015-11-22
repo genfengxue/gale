c50aae685e3cac40ef239e258f5a12a1aefc774f
lutaoact <lutaoact@hotmail.com>
2015-11-22 18:19:03 +0800

    family_album_all.json

--- scripts/convert_family_album_txt_to_json.pl
+++ scripts/convert_family_album_txt_to_json.pl
@@ -6,19 +6,21 @@ use lib 'scripts';
 use Gale qw/write_file json_encode/;
 
 my $src_path = "local_data/family_album";
-my $dst_path = "../../${src_path}_json";
+#my $dst_path = "../../${src_path}_json";
+my $json_file = "../../local_data/others/family_album_all.json";
 
 chdir $src_path or die $!;
 
 my @files = glob "*";
-@files = ($files[0]);
+#@files = ($files[0]);
 
+my $objects = [];
 for my $file (@files) {
   print "$file\n";
   my $lessonNo = substr $file, 0, 5;
 
   open my $fh, '<', $file or die $!;
-  my ($sentenceNo, $objects) = (0, []);
+  my $sentenceNo = 0;
   while (my $line = <$fh>) {
     $sentenceNo++;
     $line =~ s/^\s+|\s+$//g;#去掉首尾空白
@@ -31,6 +33,6 @@ for my $file (@files) {
 
     push @{$objects}, {%{$object}};
   }
-  print json_encode( $objects );
-#  write_file("$dst_path/$file.json", json_encode( $objects ) );
+#  print json_encode( $objects );
+  write_file($json_file, json_encode( $objects ) );
 }
